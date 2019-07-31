1ce397d77cb0fc97db80e1701ad54d886477b5da
Gary Storey <gary@digitalcrafts.com>
2019-07-30 22:02:14 -0400

    Updated avatar class and card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,3 +5,35 @@ const Card = () => {
 };
 
 export default Card;
+
+
+
+
+
+
+
+
+
+/*
+    const {image,firstName,lastName,location,email,phone} = user;
+return (
+    <section className="avatar">
+    <div className="avatar-image">
+        <img src={image} alt={`${firstName} ${lastName}`}/>
+    </div>
+    <div className="avatar-content">
+        <h2 className="avatar-header">{firstName} {lastName}</h2>
+        <div className="avatar-location">
+            {location}
+        </div>
+        <ul className="avatar-contact-list">
+            <li className="avatar-contact-list-item">
+                <a href={`mailto:${email}`}>✉</a>
+            </li>
+            <li className="avatar-contact-list-item">
+                <a href={`tel:${phone}`}>✆</a> 
+            </li>
+        </ul>
+    </div>
+</section>                     
+*/
